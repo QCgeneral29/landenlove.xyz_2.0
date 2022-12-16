00108cd5d3d770e834f42ad6a6368708568d387e
Landen Love <landenoooo@gmail.com>
2022-12-16 15:09:06 -0600

    Removed --delete-after from publish scripts. Deleted the blog on accident.

--- .publish.sh
+++ .publish.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-rsync -aur --exclude=".*" "$PWD"/ root@landenlove.xyz:/var/www/landenlove.xyz --delete-after --progress
+rsync -aur --exclude=".*" "$PWD"/ root@landenlove.xyz:/var/www/landenlove.xyz --progress
