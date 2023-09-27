cf4cfa0eb0c3b85e2d2ee6dafa9c0e6b5dfb70bb
Landen Love <landenlove@protonmail.com>
2023-09-27 09:13:28 +0900

    Updated excludes on publish script.

--- publish.sh
+++ publish.sh
@@ -1,2 +1,2 @@
-#! /bin/sh
-rsync -aur --exclude=".*" "$PWD"/ root@landenlove.xyz:/var/www/landenlove.xyz --progress
+#!/bin/sh
+rsync -aur --exclude=".git" --exclude="publish.sh" "$PWD"/ root@landenlove.xyz:/var/www/landenlove.xyz --progress
